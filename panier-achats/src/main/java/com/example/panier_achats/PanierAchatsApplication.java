3d3f0daebbbb3cb6a612948c5dfa5bfad401df3f
farahsadki <sadkifarah373@gmail.com>
2024-09-18 14:55:22 +0200

    ini com

---
+++ panier-achats/src/main/java/com/example/panier_achats/PanierAchatsApplication.java
@@ -0,0 +1,13 @@
+package com.example.panier_achats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanierAchatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanierAchatsApplication.class, args);
+	}
+
+}
---
+++ panier-achats/src/test/java/com/example/panier_achats/PanierAchatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.panier_achats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanierAchatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
